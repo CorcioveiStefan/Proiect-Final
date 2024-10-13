1a1afa4698e78399a325358640d1a4fd62a19f67
Stefan <stefancorciovei117@gmail.com>
2024-10-13 18:33:58 +0300

    checkout
    am facut un checkout cu trei conditii , una e sa fii logat , una e sa ai produse in cart si una e sa ai datele completate in formular

--- Checkout/Checkout.js
+++ Checkout/Checkout.js
@@ -39,3 +39,65 @@ function updateCheckoutDisplay() {
     ".cart-checkout .subtotal-calculated"
   ).textContent = `$${totalAmount.toFixed(2)}`;
 }
+
+let isFormComplete = false;
+
+const billInfo = document.querySelector(".left-section form");
+const inputs = billInfo.querySelectorAll("input");
+
+function checkFormCompleteness() {
+  isFormComplete = Array.from(inputs).every(
+    (input) => input.value.trim() !== ""
+  );
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("input", checkFormCompleteness);
+});
+
+checkFormCompleteness();
+
+const sendComButton = document.querySelector(".butonCheckout button");
+sendComButton.addEventListener("click", () => {
+  const isCheckoutAllowed = JSON.parse(localStorage.getItem("isLoggedIn"));
+  const cartData = JSON.parse(localStorage.getItem("checkoutProduct")) || [];
+
+  if (isCheckoutAllowed & isFormComplete & (cartData.length > 0)) {
+    trimiteComanda();
+  } else if (!isCheckoutAllowed) {
+    alert("Checkout is not allowed. Please login first.");
+  } else if (!isFormComplete) {
+    alert("Checkout is not allowed. Complete the form first.");
+  } else if (cartData.length == 0) alert("Add products to order.");
+});
+
+async function trimiteComanda() {
+  const cartData = JSON.parse(localStorage.getItem("checkoutProduct")) || [];
+
+  const continutBody = JSON.stringify({
+    order: cartData.map((produs) => produs.id),
+  });
+
+  try {
+    const response = await fetch("https://paulghiran.com/messages/order.php", {
+      headers: {
+        "Content-Type": "application/json",
+        userkey: "london",
+      },
+      method: "POST",
+      body: continutBody,
+    });
+
+    if (response.ok) {
+      alert("Order successfully placed!");
+      localStorage.removeItem("checkoutProduct");
+      localStorage.removeItem("cart");
+      location.reload();
+    } else {
+      alert("Failed to place the order. Please try again.");
+    }
+  } catch (error) {
+    alert("Error sending order. Please check your connection and try again.");
+    console.error("Error sending order:", error);
+  }
+}
